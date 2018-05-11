dee192090520d18d079dcce11328ac9932eaea02
goingchan <goingchan@gmail.com>
2018-05-12 01:29:54 +0800

    set client full page reload by query parameter __hmr_reload=true

--- template/build/dev-client.js
+++ template/build/dev-client.js
@@ -1,9 +1,13 @@
-/* eslint-disable */
+/*eslint-env browser*/
+/*global __resourceQuery __webpack_public_path__*/
 require('eventsource-polyfill')
 var hotClient = require('webpack-hot-middleware/client?noInfo=true&reload=true')
 
+var querystring = require('querystring');
+var qs = querystring.parse(document.location.search.slice(1));
 hotClient.subscribe(function (event) {
   if (event.action === 'reload') {
+    if(qs.__hmr_reload === 'true')
     window.location.reload()
   }
 })
